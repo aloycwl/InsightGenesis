96d7bfc9b3d024d08bcfaa7b1a006c9423e3f74d
aloycwl <aloycwl@gmail.com>
2025-04-14 14:56:12 +0800

    Updated Interface

--- contracts/IInsightData.sol
+++ contracts/IInsightData.sol
@@ -2,10 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IInsightData {
-    function data(uint256) external view returns (bytes memory);
+    function data(uint256) external view returns (string calldata);
     function user(uint256) external view returns (address);
     function userCount(address) external view returns (uint256);
     function count() external view returns (uint256);
     event NewData(uint256 indexed, bytes, address);
-    function store(bytes memory) external;
+    function store(string calldata, address) external;
 }
