aa8559d745a74fbce8d8573e126a4e6a97bb0c62
AC <aloycwl@gmail.com>
2025-04-14 07:10:04 +0000

    Auto

--- testevm.ts
+++ testevm.ts
@@ -2,24 +2,36 @@ import { ethers } from "ethers";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-
-const provider = new ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.bnbchain.org:8545/");
-
-const signer = new ethers.Wallet(process.env.PK!, provider);
-
-// Example: ABI + contract address
-const abi = [
-  "function myFunction(uint256 value) public view returns (uint256)", // view/pure
-  "function setValue(uint256 value) public"                            // write
-];
-const contractAddress = "0xF70068E66527294f6073bF7a39414E2B12a03C8f";
-
-const contract = new ethers.Contract(contractAddress, abi, provider);
+const pv = new ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.bnbchain.org:8545/");
+const contract = new ethers.Contract("0xF70068E66527294f6073bF7a39414E2B12a03C8f",
+  ["function store(string calldata, address) external"], pv);
 
 async function callFunction() {
-  // For read-only functions
-  const result = await contract.myFunction(42);
-  console.log("Function result:", result.toString());
+  const tx = await contract.connect(new ethers.Wallet(process.env.PK!, pv)).
+    setValue("someIPFScidthatis59byteslongjuztoadd21characters");
+  await tx.wait();
+  console.log("Transaction confirmed!");
 }
 
 callFunction();
+
+// [
+//   {
+//     "inputs": [
+//       {
+//         "internalType": "string",
+//         "name": "_data",
+//         "type": "string"
+//       },
+//       {
+//         "internalType": "address",
+//         "name": "_account",
+//         "type": "address"
+//       }
+//     ],
+//     "name": "store",
+//     "outputs": [],
+//     "stateMutability": "nonpayable",
+//     "type": "function"
+//   }
+// ]
