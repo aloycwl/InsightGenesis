8061d565509db77e9d1c914fbe9b3e5f5e8e1af0
aloycwl <aloycwl@gmail.com>
2025-04-14 13:54:29 +0800

    Changed byte to string

--- contracts/InsightData.sol
+++ contracts/InsightData.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.0;
 
 contract InsightData {
-    mapping(uint256 => bytes) public data;
+    mapping(address => string) public data;
     mapping(uint256 => address) public user;
     mapping(address => uint256) public userCount;
     uint256 public count;
-    event NewData(uint256 indexed, bytes, address);
+    event NewData(uint256 indexed, string, address);
 
-    function store(bytes memory _data) public {
+    function store(string calldata _data, address _account) external {
         unchecked {
-            (data[count], user[count]) = (_data, msg.sender);
-            ++userCount[msg.sender];
-            ++count;
-            emit NewData(count, _data, msg.sender);
+            uint _count = ++count;
+            (data[_account], user[_count]) = (_data, _account);
+            ++userCount[_account];
+            emit NewData(_count, _data, _account);
         }
     }
 }
