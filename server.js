45f2c2ee2d063b4325c2214035f6f1fdc1a370fa
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2025-04-28 06:14:04 +0000

    Nicely delivered payment mode

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import express from "express";
 import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
 import { setReferral } from "./referral.js";
 import { setPaid } from "./pay.js";
 import { upload } from "./upload.js";
 
 const ap = express();
 const ul = multer({ dest: "uploads/" });
+const dn = path.dirname(fileURLToPath(import.meta.url));
+
+ap.get("/pay", (re, rq) => {
+  rq.sendFile(path.join(dn, "public", "pay.html"));
+});
 
 ap.post("/upload", ul.single("file"), async (re, rs) => {
   try {
