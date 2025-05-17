7724495042b65ba34ec0976005ebef8edd7db2ec
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2025-05-17 10:06:47 +0000

    updated newest address

--- server/config.js
+++ server/config.js
@@ -2,8 +2,8 @@ import * as de from "dotenv";
 de.config();
 
 export const aa = "0x41034f6c38DDB166A21F1eba3dCf93de308375A6";
-export const ci = "0xe180Fed6E542e451765a738d0D2dD9c041526EE4";
-export const cr = "0x6199458E643a7B08eF6d2bFFa7f41f7df325024F";
+export const ci = "0x6e0fE143b8cDbb7287c0d73d76dB2a259569Bf89";
+export const cr = "0x9bE861104E64Af9FBFe53d13FCF8Bda63C47E3ca";
 export const ig = "https://api.insightgenie.ai/";
 export const mp = "pk_live_27266000CD95B626"
 export const pg = "amber-implicit-jay-463.mypinata.cloud";
