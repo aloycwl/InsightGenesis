827a39cd7b6d086996cc34ba6169e52832b6f5b6
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2025-05-17 09:43:37 +0000

    updated config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 import * as de from "dotenv";
 de.config();
 
-export const aa = "0xEc1F090816a04C22f97007BcDc3B4f5e247E90C4";
+export const aa = "0x41034f6c38DDB166A21F1eba3dCf93de308375A6";
 export const ci = "0xe180Fed6E542e451765a738d0D2dD9c041526EE4";
 export const cr = "0x6199458E643a7B08eF6d2bFFa7f41f7df325024F";
 export const ig = "https://api.insightgenie.ai/";
