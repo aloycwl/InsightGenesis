aefab549fb0c006ddf02c80143c892545cb55055
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2025-05-25 02:13:40 +0000

    Reduced chunk size

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
   build: {
     outDir: "build",
-  },
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react-router-dom")) return "vendor-router";
+            if (id.includes("react")) return "vendor-react";
+            return "vendor";
+          }
+        }
+      }
+    }
+  }
 });
