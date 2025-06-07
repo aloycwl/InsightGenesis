c9d9bad881a221ae9658813ecf8bc345043ebb92
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2025-06-07 14:33:30 +0000

    changed to queuing system

--- js/onchain.js
+++ js/onchain.js
@@ -1,13 +1,15 @@
 import { ci, pr, PK, D, M } from "./config.js";
 import { create } from "@web3-storage/w3up-client";
 import { dbIGAI, dbNew, dbTo, dbRef } from "./supabase.js";
-import ethers from "ethers";
-const { providers, Contract, Wallet } = ethers;
-const { JsonRpcProvider } = providers;
 import { NonceManager as N } from "@ethersproject/experimental";
+import ethers from "ethers";
 
-const w = new Wallet(PK, new JsonRpcProvider(pr)),
-  s = new N(w);
+const { providers, Contract, Wallet } = ethers,
+  { JsonRpcProvider } = providers,
+  w = new Wallet(PK, new JsonRpcProvider(pr)),
+  s = new N(w),
+  q = [];
+let p = false;
 
 export async function ref(t, f) {
   if ((await dbTo(t)) && t != f) {
@@ -18,25 +20,39 @@ export async function ref(t, f) {
   }
 }
 
-export async function store(d, ra, rt, aa) {
-  if (await dbNew(ra)) {
-    const t = await new Contract(
-      ci,
-      ["function deduct(address, address)"],
-      w.provider,
-    )
-      .connect(s)
-      .deduct(ra, aa);
-    await t.wait();
+export async function processQueue() {
+  if (p) return;
+  p = true;
+
+  while (q.length > 0) {
+    const { d, ra, rt, aa } = q.shift();
+
+    if (await dbNew(ra))
+      await (
+        await new Contract(
+          ci,
+          ["function deduct(address, address)"],
+          w.provider,
+        )
+          .connect(s)
+          .deduct(ra, aa)
+      ).wait();
+
+    const c = await create();
+    await c.login(M);
+    await c.setCurrentSpace(D);
+
+    await dbIGAI(
+      (await c.uploadFile(new File([JSON.stringify(d)], ""))).toString(),
+      ra,
+      rt,
+    );
   }
 
-  const c = await create();
-  await c.login(M);
-  await c.setCurrentSpace(D);
+  p = false;
+}
 
-  dbIGAI(
-    (await c.uploadFile(new File([JSON.stringify(d)], ""))).toString(),
-    ra,
-    rt,
-  );
+export async function store(d, ra, rt, aa) {
+  q.push({ d, ra, rt, aa });
+  processQueue();
 }
