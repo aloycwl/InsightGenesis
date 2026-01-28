477d1ec0ca88f64e50a902bc2b2547647a92b7b9
Aloysius Chan <63489563+aloycwl@users.noreply.github.com>
2026-01-28 06:11:10 +0000

    Upgraded to ENI chain

--- js/config.js
+++ js/config.js
@@ -1,12 +1,27 @@
 import * as de from "dotenv";
 de.config();
 
+// admin address
 export const aa = "0x41034f6c38DDB166A21F1eba3dCf93de308375A6";
-export const ci = "0x7d1fe42532cEE53A23cc266c06Ac55e65b0797a9";
-export const cr = "0x0C1A6816C7C59C2876624d0AdBd53Eb9bb6291bc";
-export const pr = "https://v2-mainnet-rpc.l1x.foundation/";
+
+// ENI Mainnet
+export const ci = "0xa56077844e7Aa7DCcAd3b264E683f2F888007f70";
+export const cr = "0xf42cE624ff5A4aC41afdb3e85108672ba843A1d0";
+export const pr = "https://rpc.eniac.network";
+
+// Supabase, Web3.storage
 export const su = "https://uxxfyiukhlsahcyszutt.supabase.co";
 export const D = "did:key:z6MkrP3iUSsWXTvYsVEHh7ZKEBnJ7smyut1Av13kw5d36oL3";
 export const M = "aloycwl@gmail.com";
 
 export const { GH, IK, IS, MA, PK, SB } = process.env;
+
+// L1x mainnet
+// export const ci = "0x7d1fe42532cEE53A23cc266c06Ac55e65b0797a9";
+// export const cr = "0x0C1A6816C7C59C2876624d0AdBd53Eb9bb6291bc";
+// export const pr = "https://v2-mainnet-rpc.l1x.foundation/";
+
+// BSC testnet
+// export const ci = "0x82043904A70a6ED4dE326be70dc65E6d9416bedC";
+// export const cr = "0x7A0Ee6cB11479b3aBC5DFC5D86aD9b74F751A0B7";
+// export const pr = "https://data-seed-prebsc-2-s3.bnbchain.org:8545";
